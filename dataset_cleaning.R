0e4f38650c40835c4914c533c4b345e59c46fcce
Krystian Gronek <k.gronek3@student.uw.edu.pl>
2022-12-29 14:41:43 +0100

    added codes for land and ocean temperature data set cleaning

--- dataset_cleaning.R
+++ dataset_cleaning.R
@@ -22,6 +22,24 @@ ice_south <- read.csv("./data/S_seaice.csv", sep = ",") %>%
 # Daily Surface Melt Extent ####
 # http://nsidc.org/greenland-today/greenland-today-data-and-analysis-tools/
 
+ice_melt <- read.xlsx("./data/greenland-daily-melt.xlsx", sheet = 1) %>% 
+    select(-X47,-X48) %>% 
+    pivot_longer(!c(month,day), names_to = "year", values_to = "IceMelt") %>% 
+    mutate(month2 = case_when(month == "March" ~ "03",
+                              month == "April" ~ "04",
+                              month == "May" ~ "05",
+                              month == "June" ~ "06",
+                              month == "July" ~ "07",
+                              month == "August" ~ "08",
+                              month == "September" ~ "09",
+                              month == "October" ~ "10"),
+           day2 = case_when(day < 10 ~ paste0("0",day),
+                            day >= 10 ~ as.character(day))) %>% 
+    mutate(date = paste0(year,"-",month2,"-",day2)) %>% 
+    mutate_at("date", as.Date, format = "%Y-%m-%d") %>% 
+    select(date,IceMelt) %>% 
+    arrange(date)
+
 
 # Land (1910-2022 and 1880-2022 for global temp) Ocean temperatures (1880-2022)
 # https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/global/time-series/southAmerica/land/all/11/1880-2022
@@ -85,7 +103,5 @@ land_temps <- left_join(tl_global,tl_africa, by = 'date') %>%
 
 
 remove(tl_africa, tl_NA, tl_SA, tl_oceania, tl_asia, tl_europe, tl_global)
-
-
 
 
