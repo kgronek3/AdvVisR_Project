a3f354c48d4adcb08d16c986f14c5767dc090d67
Krystian Gronek <k.gronek3@student.uw.edu.pl>
2022-12-29 14:38:48 +0100

    added codes for daily surface melt extend dataset cleaning

--- dataset_cleaning.R
+++ dataset_cleaning.R
@@ -1,5 +1,6 @@
 #### Dataset loading and cleaning script ####
 library(tidyverse)
+library(openxlsx)
 
 # Sea ice extent Dataset
 # https://nsidc.org/data/geophysical-measurements
@@ -10,10 +11,6 @@ ice_north <- read.csv("./data/N_seaice.csv", sep = ",") %>%
     mutate_at("date", as.Date, format = "%Y-%m-%d") %>% 
     select(-Source.Data, -Missing) %>% 
     filter(!is.na(Extent))
-
-
-head(ice_north)
-tail(ice_north)
     
 ice_south <- read.csv("./data/S_seaice.csv", sep = ",") %>% 
     unite("date", Year:Day, sep = "-", remove = TRUE) %>% 
@@ -21,11 +18,74 @@ ice_south <- read.csv("./data/S_seaice.csv", sep = ",") %>%
     select(-Source.Data, -Missing) %>% 
     filter(!is.na(Extent))
 
-head(ice_south)
-tail(ice_south)
 
-ggplot(data = ice_north) +
-    geom_line(aes(x = date, y = Extent))
+# Daily Surface Melt Extent ####
+# http://nsidc.org/greenland-today/greenland-today-data-and-analysis-tools/
 
-ggplot(data = ice_south) +
-    geom_line(aes(x = date, y = Extent))
+
+# Land (1910-2022 and 1880-2022 for global temp) Ocean temperatures (1880-2022)
+# https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/global/time-series/southAmerica/land/all/11/1880-2022
+
+# temperature of the ocean - Global
+to_global <- read.csv("./data/temp_global_ocean.csv", sep = ",") %>% 
+    mutate(date = paste0(substr(.$Year, 1, 4),"-",substr(.$Year, 5, 6),"-","01")) %>% 
+    select(3,2) %>% 
+    mutate_at("date", as.Date, format = "%Y-%m-%d")
+
+plot(x = to_global$date, y = to_global$Global_Ocean, type = "l")
+
+# land temperatures for 6 continents + global land temperature
+tl_global <- read.csv("./data/temp_global_land.csv", sep = ",") %>% 
+    mutate(date = paste0(substr(.$Year, 1, 4),"-",substr(.$Year, 5, 6),"-","01")) %>% 
+    select(3,2) %>% 
+    mutate_at("date", as.Date, format = "%Y-%m-%d")
+
+tl_africa <- read.csv("./data/temp_africa_land.csv", sep = ",") %>% 
+    mutate(date = paste0(substr(.$Year, 1, 4),"-",substr(.$Year, 5, 6),"-","01")) %>% 
+    select(3,2) %>% 
+    mutate_at("date", as.Date, format = "%Y-%m-%d")
+
+tl_NA <- read.csv("./data/temp_NA_land.csv", sep = ",") %>% 
+    mutate(date = paste0(substr(.$Year, 1, 4),"-",substr(.$Year, 5, 6),"-","01")) %>% 
+    select(3,2) %>% 
+    mutate_at("date", as.Date, format = "%Y-%m-%d")
+
+tl_SA <- read.csv("./data/temp_SA_land.csv", sep = ",") %>% 
+    mutate(date = paste0(substr(.$Year, 1, 4),"-",substr(.$Year, 5, 6),"-","01")) %>% 
+    select(3,2) %>% 
+    mutate_at("date", as.Date, format = "%Y-%m-%d")
+
+tl_oceania <- read.csv("./data/temp_oceania_land.csv", sep = ",") %>% 
+    mutate(date = paste0(substr(.$Year, 1, 4),"-",substr(.$Year, 5, 6),"-","01")) %>% 
+    select(3,2) %>% 
+    mutate_at("date", as.Date, format = "%Y-%m-%d")
+
+tl_asia <- read.csv("./data/temp_asia_land.csv", sep = ",") %>% 
+    mutate(date = paste0(substr(.$Year, 1, 4),"-",substr(.$Year, 5, 6),"-","01")) %>% 
+    select(3,2) %>% 
+    mutate_at("date", as.Date, format = "%Y-%m-%d")
+
+tl_asia <- read.csv("./data/temp_asia_land.csv", sep = ",") %>% 
+    mutate(date = paste0(substr(.$Year, 1, 4),"-",substr(.$Year, 5, 6),"-","01")) %>% 
+    select(3,2) %>% 
+    mutate_at("date", as.Date, format = "%Y-%m-%d")
+
+tl_europe <- read.csv("./data/temp_europe_land.csv", sep = ",") %>% 
+    mutate(date = paste0(substr(.$Year, 1, 4),"-",substr(.$Year, 5, 6),"-","01")) %>% 
+    select(3,2) %>% 
+    mutate_at("date", as.Date, format = "%Y-%m-%d")
+
+
+land_temps <- left_join(tl_global,tl_africa, by = 'date') %>%
+    left_join(tl_NA,by = "date") %>% 
+    left_join(tl_SA,by = "date") %>% 
+    left_join(tl_oceania,by = "date") %>% 
+    left_join(tl_asia,by = "date") %>% 
+    left_join(tl_europe,by = "date")
+
+
+remove(tl_africa, tl_NA, tl_SA, tl_oceania, tl_asia, tl_europe, tl_global)
+
+
+
+
